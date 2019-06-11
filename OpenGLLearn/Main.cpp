6c3344bbfd798c4d2b0de04830cdfc930117e382
Dragonljx <dragonljx@126.com>
2019-06-11 14:05:54 +0800

    默认上传

---
+++ OpenGLLearn/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//glfwWindowHint();
+
+}
